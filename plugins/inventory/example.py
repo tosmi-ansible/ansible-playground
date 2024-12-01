5824b63e348bf664adf1b23836ff8f42d1fbb624
Toni Schmidbauer <toni@stderr.at>
2024-12-01 16:22:06 +0100

    Add example inventory plugin and playbook
    Introduced an example inventory plugin that provides a static list of hosts. Created a playbook to print the current inventory for localhost.

--- plugins/inventory/example.py
+++ plugins/inventory/example.py
@@ -1,4 +1,32 @@
 from ansible.plugins.inventory import BaseInventoryPlugin
+from ansible_collections.awx.awx.plugins.lookup.schedule_rruleset import DOCUMENTATION, EXAMPLES
+
+DOCUMENTATION = r"""
+    name: example
+    short_description: Example inventory plugin
+    description:
+        - This is an example of an inventory plugin.
+        - It returns a static list of hosts
+"""
+
+EXAMPLES = r"""
+    # this inventory returns an example group with two hosts
+"""
+
 
 class InventoryModule(BaseInventoryPlugin):
     NAME = 'example'
+
+    def verify_file(self, path):
+        return path.endswith(('example.yml', 'example.yaml'))
+
+    def parse(self, inventory, loader, path, cache=True):
+        inventory.add_host('example-host1')
+        inventory.add_host('example-host2')
+        inventory.add_host('example-host3')
+
+        inventory.add_group('example-group')
+        inventory.add_child('example-group', 'example-host1')
+        inventory.add_child('example-group','example-host2')
+
+
